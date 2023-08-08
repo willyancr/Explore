7e075c166016ad0a69f60c6f17d40612fe3fbd3b
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-08-08 14:58:56 -0300

    up

--- JavaScript/jogo-adivinhacao/main.js
+++ JavaScript/jogo-adivinhacao/main.js
@@ -1,23 +1,34 @@
-const numRandon = Math.floor(Math.random() * 3)
-let xAttempts = 1
-// const button = document.querySelector('button')
 
-function handleClick(event){
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const numRandon = Math.floor(Math.random() * 5)
+let xAttempts = 1
+
+function handleClick(event){ //função verifica se o num digitado é igual ao randon gerado
     event.preventDefault() //Não faz o envio de formulário padrão
     
     const inputNumber = document.querySelector('#inputNumber')
     
     if(Number(inputNumber.value) == numRandon){
-        document.querySelector('.screen1').classList.add('hide')
-        document.querySelector('.screen2').classList.remove('hide')
+        screen1.classList.add('hide')
+        screen2.classList.remove('hide')
         
-        document.querySelector('.screen2 h2').innerText = `Acertou em ${xAttempts} tentativas`
-        
-        
+        document.querySelector('.screen2 h2').innerText = `Acertou em ${xAttempts} tentativas`    
     }
-    // button.addEventListener('click', function(){
-    //     document.querySelector('.screen2').classList.add('hide')
-    //     document.querySelector('.screen1').classList.remove('hide')
-    // })
+    
+    inputNumber.value = "" // Limpar o campo digitado
     xAttempts++
 }
+
+//Eventos
+const btnTry = document.querySelector('#btnTry')
+const btnPlayAgain = document.querySelector('#btnPlayAgain')
+
+btnTry.addEventListener('click', handleClick) // ao clicar no botão tentar, chama a função handleClick
+btnPlayAgain.addEventListener('click', function(){ // ao clicar no botão jogar novamente, volta pra primeira pagina
+
+    screen1.classList.remove('hide')
+    screen2.classList.add('hide')
+    xAttempts = 1
+
+})
