ed4f63835b3051b05ac5e256393d236aae459f37
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-08-18 17:14:41 -0300

    up

--- JavaScript/aplicativo-foco/js/sound.js
+++ JavaScript/aplicativo-foco/js/sound.js
@@ -1,6 +1,7 @@
 export default () => {
-    const alarmAudio = new Audio('https://github.com/willyancr/Explore/blob/main/JavaScript/aplicativo-foco/music/Bounce%20-%20Huawei%20Ringtone.mp3')
-    const bgAudio = new Audio('./music/musicFocus.mp3')
+    const alarmAudio = new Audio('https://drive.google.com/file/d/1nWoyUfGoZPX5bEMBV6x825fM0Vd9Nz3b/view?usp=sharing')
+    const bgAudio = new Audio('https://github.com/willyancr/Explore/blob/main/JavaScript/aplicativo-foco/music/musicFocus.mp3?raw=true')
+    //const bgAudio = new Audio('./music/musicFocus.mp3')
 
     function alarm(){
         alarmAudio.play()
