22d569f4e94624d3bb10b7dcda34966b8dc37143
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-08-31 23:44:39 -0300

    up

--- JavaScript/github-favorites/js/favorites.js
+++ JavaScript/github-favorites/js/favorites.js
@@ -1,19 +1,4 @@
-//classe api github
-export class apiGitHub{
-    static search(username){
-        const api = `https://api.github.com/users/${username}`
-
-        return fetch(api).then(data => data.json())
-        .then(data => 
-                ({
-                    login: data.login,
-                    name: data.name,
-                    public_repos: data.public_repos,
-                    followers: data.followers
-                })
-            )
-    }
-}
+import { apiGitHub } from "./apiGitHub.js"
 
 //classe que vai conter a lógica dos dados - como os dados serão estruturados
 export class Favorites{
